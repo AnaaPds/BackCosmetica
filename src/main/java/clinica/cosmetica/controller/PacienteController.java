cadb7f7de0a3fbf8bd93b2e7047f88b9a39c968e
AnaaPds <anapauladantas1804@gmail.com>
2025-06-08 21:33:05 -0300

    Update PacienteController.java

--- src/main/java/clinica/cosmetica/controller/PacienteController.java
+++ src/main/java/clinica/cosmetica/controller/PacienteController.java
@@ -2,14 +2,14 @@ package clinica.cosmetica.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.*;
-=======
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
 
 import clinica.cosmetica.entities.Paciente;
 import clinica.cosmetica.service.PacienteService;
@@ -18,27 +18,26 @@ import clinica.cosmetica.service.PacienteService;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
-<<<<<<< HEAD
+
     // Injeta o serviço de pacientes
     @Autowired
     private PacienteService pacienteService;
 
     // Cadastra um paciente novo
-=======
+
     @Autowired
     private PacienteService pacienteService;
 
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
     @PostMapping("/cadastro")
     public ResponseEntity<Paciente> cadastrarPaciente(@RequestBody Paciente paciente) {
         Paciente salvo = pacienteService.salvar(paciente);
         return ResponseEntity.ok(salvo);
     }
 
-<<<<<<< HEAD
+
     // Realiza autenticação do paciente
-=======
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
     @PostMapping("/login")
     public ResponseEntity<?> loginPaciente(@RequestBody Paciente paciente) {
         String token = pacienteService.autenticar(paciente.getEmail(), paciente.getSenha());
@@ -48,8 +47,4 @@ public class PacienteController {
             return ResponseEntity.status(401).body("Email ou senha inválidos");
         }
     }
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
