6200e6391dac5f9e2ae5d3bf5fe5e3d5e6a2b10e
AnaaPds <anapauladantas1804@gmail.com>
2025-06-08 21:32:25 -0300

    Update MedicoController.java

--- src/main/java/clinica/cosmetica/controller/MedicoController.java
+++ src/main/java/clinica/cosmetica/controller/MedicoController.java
@@ -2,14 +2,14 @@ package clinica.cosmetica.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.*;
-=======
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
 
 import clinica.cosmetica.entities.Medico;
 import clinica.cosmetica.service.MedicoService;
@@ -18,27 +18,26 @@ import clinica.cosmetica.service.MedicoService;
 @RequestMapping("/profissionais")
 public class MedicoController {
 
-<<<<<<< HEAD
+
     // Injeta o serviço de médicos
     @Autowired
     private MedicoService medicoService;
 
     // Cadastra um novo médico
-=======
+
     @Autowired
     private MedicoService medicoService;
 
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
     @PostMapping("/cadastro")
     public ResponseEntity<Medico> cadastrarMedico(@RequestBody Medico medico) {
         Medico salvo = medicoService.salvar(medico);
         return ResponseEntity.ok(salvo);
     }
 
-<<<<<<< HEAD
+
     // Realiza login do médico
-=======
->>>>>>> af3506727525665f87d5b676e754d1c05ce51add
+
     @PostMapping("/login")
     public ResponseEntity<?> loginMedico(@RequestBody Medico medico) {
         String token = medicoService.autenticar(medico.getEmail(), medico.getSenha());
